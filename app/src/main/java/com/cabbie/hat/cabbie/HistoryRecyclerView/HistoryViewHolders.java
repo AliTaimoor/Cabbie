2e35841f50ffd6e0f647ebcd1b0e42f157ba527c
Ali Taimoor <alitaimoor589@gmail.com>
2018-12-16 16:17:15 +0500

    Selecting a ride history

--- app/src/main/java/com/cabbie/hat/cabbie/HistoryRecyclerView/HistoryViewHolders.java
+++ app/src/main/java/com/cabbie/hat/cabbie/HistoryRecyclerView/HistoryViewHolders.java
@@ -1,9 +1,12 @@
 package com.cabbie.hat.cabbie.HistoryRecyclerView;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.cabbie.hat.cabbie.HistorySingleActivity;
 import com.cabbie.hat.cabbie.R;
 
 public class HistoryViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
@@ -17,5 +20,12 @@ public class HistoryViewHolders extends RecyclerView.ViewHolder implements View.
     }
     @Override
     public void onClick(View v) {
+
+        Intent intent = new Intent(v.getContext(), HistorySingleActivity.class);
+        Bundle b = new Bundle();
+        b.putString("rideId", rideId.getText().toString());
+        intent.putExtras(b);
+        v.getContext().startActivity(intent);
+
     }
 }
